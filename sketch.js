2dbecfd98a38d07036aeba7c1de16954ee161d74
FelipeFadil <felipefadil@gmail.com>
2020-07-13 16:43:07 -0300

    Add som do jogo

--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
 function setup() {
     createCanvas(windowWidth, windowHeight)
     frameRate(40)
-    //somDoJogo.loop()
+    somDoJogo.loop()
     jogo = new Jogo()
     telaInicial = new TelaInicial()
     botaoGerenciador = new BotaoGerenciador('Iniciar', width/2, height/2)
